836695d199c5f8b1a73548fe0d20567048d0ed2a
Maximilian Pfundstein <maximilian@pfundstein.org>
2018-09-30 13:48:55 +0200

    First implementation of dropdown

--- R/server.R
+++ R/server.R
@@ -11,22 +11,15 @@ mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
 # Define server logic required to plot various variables against mpg
 shinyServer(function(input, output) {
   
-  # Compute the forumla text in a reactive expression since it is 
-  # shared by the output$caption and output$mpgPlot expressions
-  formulaText <- reactive({
-    paste("mpg ~", input$variable)
+  municipalitiesDataFrame = fetchMunicipalities()
+  
+  # Municipality Dropdown
+  output$municipalityDropDownList <- renderUI({
+    selectInput(inputId = "municipalityDropDownList",
+                choices = as.matrix(municipalitiesDataFrame["values.title"]),
+                label = "This is the label")
   })
   
-  # Return the formula text for printing as a caption
-  output$caption <- renderText({
-    formulaText()
-  })
   
-  # Generate a plot of the requested variable against mpg and only 
-  # include outliers if requested
-  output$mpgPlot <- renderPlot({
-    boxplot(as.formula(formulaText()), 
-            data = mpgData,
-            outline = input$outliers)
-  })
+  
 })
--- R/ui.R
+++ R/ui.R
@@ -1,24 +1,47 @@
-# Define UI for miles per gallon application
-shinyUI(pageWithSidebar(
-  
-  # Application title
-  headerPanel("Miles Per Gallon"),
-  
-  # Sidebar with controls to select the variable to plot against mpg
-  # and to specify whether outliers should be included
-  sidebarPanel(
-    selectInput("variable", "Variable:",
-                list("Cylinders" = "cyl", 
-                     "Transmission" = "am", 
-                     "Gears" = "gear")),
-    
-    checkboxInput("outliers", "Show outliers", FALSE)
-  ),
-  
-  # Show the caption and plot of the requested variable against mpg
-  mainPanel(
-    h3(textOutput("caption")),
-    
-    plotOutput("mpgPlot")
+library(shiny)
+library(ggplot2)
+library(httr)
+library(jsonlite)
+source('webService.R')
+
+shinyUI(
+  fluidPage(
+    fluidRow(
+      column(12,
+             "Comparison of two plots",
+             fluidRow(
+               column(6,
+                      " Select Input",
+                      fluidRow(
+                        column(4, 
+                               "Dropdown Input 1",
+                               uiOutput("municipalityDropDownList")
+                        ),
+                        column(4,
+                               "Dropdown Input 2"),
+                        column(4,
+                               "Dropdown Input 3")
+                      ),
+                      fluidRow(
+                        plotOutput("Barplot_1")
+                      )
+               ),
+               column(6,
+                      " Select Input",
+                      fluidRow(
+                        column(4, 
+                               "Dropdown Input 1"),
+                        column(4,
+                               "Dropdown Input 2"),
+                        column(4,
+                               "Dropdown Input 3")
+                      ),
+                      fluidRow(
+                        plotOutput("Barplot 2")
+                      )
+               )
+             )
+      )
+    )
   )
-))
+)
