7fdd7c0e93b8e5132af0631b2065bdf79751d665
Maximilian Pfundstein <maximilian@pfundstein.org>
2018-10-05 13:33:07 +0200

    Refactor code

--- R/app.R
+++ R/app.R
@@ -1,3 +1,15 @@
+#library(shiny)
+#library(httr)
+#library(jsonlite)
+#library(shinythemes)
+#library(ggplot2)
+#library(yarrr)
+
+require(shiny)
+require(shinythemes)
+
+#source('R/webService.R')
+
 # Call the recover function when an error occurs
 options(error = recover)
 
@@ -334,3 +346,68 @@ server = function(input, output) {
     }
   })
 }
+
+ui = shinyUI(fluidPage( theme = shinytheme("slate"), fluidRow(column(
+  12,
+  h1("Kola database", align = "center"),
+  h3("KPI comparison of two municipalities", align = "center"),
+  hr(),
+  fluidRow(
+    column(
+      6,
+      
+      wellPanel(
+        h4(" 1. Select Parameters"),
+        hr(),
+        column(6,
+               uiOutput("municipalityDropDownListLeft")),
+        column(6,
+               uiOutput("kpiDropDownListLeft")),
+        sliderInput(
+          "yearDropDownListLeft",
+          label = "Year Range",
+          min = 1980,
+          max = 2017,
+          value = c(1992, 2004),
+          width = "100%"
+        ),
+        fluidRow( align = "center",
+                  actionButton(
+                    inputId = "PlotButtonLeft",
+                    label = "2. Plot dat shit!"
+                  )
+        )
+      ),
+      fluidRow(plotOutput("Barplot_Left"))
+      
+    ),
+    column(
+      6,
+      wellPanel(
+        h4(" 1. Select Parameters"),
+        hr(),
+        column(6,
+               uiOutput("municipalityDropDownListRight")),
+        column(6,
+               uiOutput("kpiDropDownListRight")),
+        sliderInput(
+          "yearDropDownListRight",
+          label = "Year Range",
+          min = 1980,
+          max = 2017,
+          value = c(1992, 2004),
+          width = "100%"
+        ),
+        fluidRow( align = "center",
+                  actionButton(
+                    inputId = "PlotButtonRight",
+                    label = "2. Plot dat shit!"
+                  )
+        )
+      ),
+      fluidRow(plotOutput("Barplot_Right"))
+    )
+  )
+))))
+
+shinyApp(ui = ui, server = server)
